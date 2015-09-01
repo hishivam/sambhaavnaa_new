7cbe97718697f49fcdad99608ce9681f68e6444c
Naved Khan <navidkhn1@gmail.com>
2015-09-01 18:39:49 +0530

    Updates
    Updates

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+$("#slides").slidesjs({
+        width: 940,
+        height: 528,
+        navigation: {
+      active: false,
+      effect: "slide"
+    },
+    pagination: {
+      active: false,
+      effect: "slide"
+    },
+        play: {
+      active: false,
+        // [boolean] Generate the play and stop buttons.
+        // You cannot use your own buttons. Sorry.
+      effect: "slide",
+        // [string] Can be either "slide" or "fade".
+      interval: 3000,
+        // [number] Time spent on each slide in milliseconds.
+      auto: true,
+        // [boolean] Start playing the slideshow on load.
+      swap: true,
+        // [boolean] show/hide stop and play buttons
+      pauseOnHover: false,
+        // [boolean] pause a playing slideshow on hover
+      restartDelay: 2500
+        // [number] restart delay on inactive slideshow
+    }
+      });
